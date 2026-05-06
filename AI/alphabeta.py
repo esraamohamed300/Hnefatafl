0e7f8e0f2c01239337757f5d532a3c466af8a6b8
esraamohamed300 <esra2.mohamed2006@gmail.com>
2026-05-06 21:39:28 +0300

    last version

--- AI/alphabeta.py
+++ AI/alphabeta.py
@@ -1,3 +1,4 @@
+
 from Core.game_state import GameState
 from AI.evaluation import evaluate
 
@@ -13,10 +14,6 @@ def alphabeta(state: GameState, depth: int, alpha: float, beta: float,
 
     legal_moves = state.get_legal_moves()
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a48031c (Last Version)
     if not legal_moves:
         return evaluate(state, ai_player)
 
@@ -31,10 +28,6 @@ def alphabeta(state: GameState, depth: int, alpha: float, beta: float,
             max_score = max(max_score, score)
             alpha = max(alpha, score)
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a48031c (Last Version)
             if beta <= alpha:
                 break
 
@@ -50,32 +43,14 @@ def alphabeta(state: GameState, depth: int, alpha: float, beta: float,
 
             min_score = min(min_score, score)
             beta = min(beta, score)
-<<<<<<< HEAD
 
-            
-=======
->>>>>>> a48031c (Last Version)
             if beta <= alpha:
                 break
 
         return min_score
 
-
-<<<<<<< HEAD
-def get_best_move(state: GameState, depth:
-                int, ai_player: str):
-    """
-    Calls alphabeta for every legal move and returns the best one.
-    
-    depth is set by difficulty:
-    Easy   → depth 1
-    Medium → depth 3
-    Hard   → depth 5
-    """
-=======
 def get_best_move(state: GameState, depth: int, ai_player: str):
   
->>>>>>> a48031c (Last Version)
     best_move  = None
     best_score = -INF
     alpha      = -INF
@@ -85,11 +60,6 @@ def get_best_move(state: GameState, depth: int, ai_player: str):
 
     for move in legal_moves:
         new_state = state.apply_move(move)
-
-<<<<<<< HEAD
-
-=======
->>>>>>> a48031c (Last Version)
         score = alphabeta(new_state, depth - 1, alpha, beta, False, ai_player)
 
         if score > best_score:
